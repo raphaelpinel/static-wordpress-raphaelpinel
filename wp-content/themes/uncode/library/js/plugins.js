d1965e18b13a764cdaa36131e96e822b38f68144
Raphaël Pinel <Raphael.Pinel@etteplanmore.com>
2019-06-03 15:45:20 +0300

    remove search in header and update

--- wp-content/themes/uncode/library/js/plugins.js
+++ wp-content/themes/uncode/library/js/plugins.js
@@ -14330,7 +14330,7 @@ jQuery.extend( jQuery.easing,
 			url = target.attr('href');
 
 		if (url) {
-			id = url.match(/(https:|https:|)\/\/(player.|www.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(\&\S+)?/);
+			id = url.match(/(http:|https:|)\/\/(player.|www.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(\&\S+)?/);
 
 			if (id[3].indexOf('youtu') > -1) {
 				type = 'youtube';
